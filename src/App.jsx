63e19d0fdc22c47a9ca30ce090bfd282e0565ed6
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-14 14:04:50 +0200

    feat(app): add a list with a toggle button each object

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { NameList } from "./components/NameList/NameList";
 import { ProductList } from "./components/ProductList/ProductList";
 import { RemoveItemList } from "./components/RemoveItemList/RemoveItemList";
 import { TaskList } from "./components/TaskList/TaskList";
+import { ToggleItemList } from "./components/ToggleItemList/ToggleItemList";
 
 export const App = () => {
 	return (
@@ -15,6 +16,7 @@ export const App = () => {
 			<AddFruitForm />
 			<RemoveItemList />
 			<DynamicShoppingList />
+			<ToggleItemList />
 		</div>
 	);
 };
---
+++ src/components/ToggleItemList/ToggleItemList.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./ToggleItemList.css";
+
+const toggles = [
+	{ id: 1, label: "Notificaciones", isActive: false },
+	{ id: 2, label: "Modo oscuro", isActive: false },
+	{ id: 3, label: "Sonido", isActive: false },
+	{ id: 4, label: "Ubicación", isActive: false },
+	{ id: 5, label: "Actualizaciones automáticas", isActive: false },
+	{ id: 6, label: "Ahorro de energía", isActive: false },
+	{ id: 7, label: "Bluetooth", isActive: false },
+	{ id: 8, label: "Wi-Fi", isActive: false },
+	{ id: 9, label: "Datos móviles", isActive: false },
+	{ id: 10, label: "Sincronización", isActive: false },
+];
+
+export const ToggleItemList = () => {
+	const [list, setList] = useState(toggles);
+
+	const onButtonClick = (id) => {
+		const selectedItem = list.find((item) => item.id === id);
+		if (!selectedItem) return;
+		const { isActive } = selectedItem;
+		const newItem = { ...selectedItem, isActive: !isActive };
+
+		const filteredList = list.filter((item) => item.id !== id);
+		const newList = [...filteredList, newItem];
+		setList(newList);
+	};
+
+	return (
+		<div className="exercise-container">
+			<h2>Listado de Activo/Inactivo</h2>
+			<ul className="list-item">
+				{list
+					.sort((itemA, itemB) => itemB.id - itemA.id)
+					.map((item) => (
+						<li key={item.id} className={item.isActive ? "active-item product-item" : "product-item"}>
+							<h4>{item.label}</h4>
+							<button onClick={() => onButtonClick(item.id)} className="btn">
+								{item.isActive ? "Desactivar" : "Activar"}
+							</button>
+						</li>
+					))}
+			</ul>
+		</div>
+	);
+};
