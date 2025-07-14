7d10da21f2bcf946b0e9067bcca38225054f0b0f
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-14 11:51:44 +0200

    feat(app): change input on <li> to improve functions and render, also add another array for completed task

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,35 +2,63 @@ import { useState } from "react";
 import { tasks } from "../../utils/tasks/tasks";
 import "./TaskList.css";
 
+const completedTasks = [];
+
 export const TaskList = () => {
 	const [tasksToDo, setTasksToDo] = useState(tasks);
-	const [done, setDone] = useState(false);
+	const [tasksDone, setTasksDone] = useState(completedTasks);
 
-	const handleInputChange = (event) => {
-		const { checked } = event.target;
+	const handleUnDoneTask = (id) => {
+		const currentTask = tasksToDo.find((task) => task.id === id);
+		const filteredTasks = tasksToDo.filter((task) => task.id !== id);
 
-		console.log("Cambiando el input");
+		setTasksToDo(filteredTasks);
+		setTasksDone((prev) => [...prev, currentTask]);
+	};
+
+	const handleDoneTask = (id) => {
+		const currentTask = tasksDone.find((task) => task.id === id);
+		const filteredTasks = tasksDone.filter((task) => task.id !== id);
+
+		setTasksDone(filteredTasks);
+		setTasksToDo((prev) => [...prev, currentTask]);
 	};
 
 	return (
 		<div className="exercise-container">
 			<h2 className="title">Lista de tareas</h2>
-			<ul className="list-item">
-				{tasksToDo.map((tarea) => {
-					return (
-						<li key={tarea.id} className={done ? "item" : "item task-to-do"}>
-							<label htmlFor={tarea.id}>{tarea.title}</label>
-							<input
-								type="checkbox"
-								name={tarea.title}
-								id={tarea.id}
-								value={done}
-								onChange={handleInputChange}
-							/>
-						</li>
-					);
-				})}
-			</ul>
+			{tasksToDo.length > 0 && (
+				<div className="list-container">
+					<h3 className="subtitle">Tareas por hacer</h3>
+					<ul className="list-item">
+						{tasksToDo.map((task) => {
+							return (
+								<li
+									key={task.id}
+									className="not-done-task item"
+									onClick={() => handleUnDoneTask(task.id)}
+								>
+									<h4>{task.title}</h4>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			)}
+			{tasksDone.length > 0 && (
+				<div className="list-container">
+					<h3 className="subtitle">Tareas completadas</h3>
+					<ul className="list-item">
+						{tasksDone.map((task) => {
+							return (
+								<li key={task.id} className="done-task item" onClick={() => handleDoneTask(task.id)}>
+									<h4>{task.title}</h4>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 };
